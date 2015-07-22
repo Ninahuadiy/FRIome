40d37d73c409d2242a88c1acf9ef64aba374e26e
Ninahuadiy <nhua8271@gmail.com>
2015-07-22 13:28:33 -0500

    2nd commit

--- sl-Phylum.R
+++ sl-Phylum.R
@@ -20,3 +20,18 @@ text(pca.phy$rotation, row.names(pca.phy$rotation))
 #Gives a plot of pca.phylum's score
 plot(pca.phy$x, xlab = "PC1", ylab = "PC2")
 text(pca.phy$x, row.names(pca.phy$x))
+
+
+# Using qplot from the ggplot2 package to plot the pca.phylum's loadings 
+# Load the ggplot2 package beforehand
+pcaPhyLoad <- pca.phy$rotation
+p <- qplot(PC1, PC2, data = pcaPhyLoad, geom = "point") + geom_text(mapping = NULL, pcaPhyLoad, label=rownames(pcaPhyLoad))
+
+# Add jitter in order to spread the points out slightly 
+p + geom_point(position = "jitter")
+
+#print plot
+p
+
+
+
