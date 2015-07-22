a369f1b6f7f9caac019549d0bc5723ea24109cbd
Ninahuadiy <nhua8271@gmail.com>
2015-07-22 16:49:26 -0500

    1st commit

---
+++ phylumPercent.R
@@ -0,0 +1,26 @@
+# creates a new dataframe minus the id column
+phylum1.num <- phylum1[,-1]
+# find row sums 
+rowSums(phylum1.num)
+# finds the percentages of row sums for entire table
+# to find for individual rows, add the phylum1.num[row#,]/rowSums(phylum1.num[row#,])*100
+rowsumPhylum <- phylum1.num/rowSums(phylum1.num)*100
+rowsumPhylum
+# combines the id column and the test table
+cbind(phylum1[,1], rowsumPhylum)
+phylumPercent <- cbind(phylum1[,1], rowsumPhylum)
+
+# renaming the id column name
+colnames(phylumPercent)[1] <- "id"
+
+#linear model
+fit <- lm(phylumPercent[1,], phylumPercent[2,])
+summary(fit)
+#plot regression against columns
+plot(phylumPercent$pFirmicutes, phylumPercent$pProteobacteria)
+
+
+#draw a correlogram 
+library(corrgram)
+corrgram(phylumPercent, upper.panel = panel.pts)
+
