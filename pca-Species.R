ee5fd947d34db9df6b698e66871e613b92a135b2
Ninahuadiy <nhua8271@gmail.com>
2015-07-29 16:07:24 -0500

    2nd commit

--- pca-Species.R
+++ pca-Species.R
@@ -7,7 +7,7 @@ names(rowsumSpecies[, sapply(rowsumSpecies, function(v) var(v, na.rm=TRUE)--0)])
 #drop constant columns
 rowsumSpecies1 <- rowsumSpecies[, sapply(rowsumSpecies, function(v) var(v, na.rm = TRUE)!=0)]
 #pca
-pca.species <- prcomp(rowsumSpecies, center = TRUE, scale. = TRUE)
+pca.species <- prcomp(rowsumSpecies1, center = TRUE, scale. = TRUE)
 
 # prints out the standard deviations and rotations of pca.phy
 print(pca.species)
@@ -29,6 +29,48 @@ pca.species$x
 
 # turns it into a data frame, so that we don't have to type out pca.phy$x
 pcaSpeciesData <- as.data.frame(pca.species$x)
+# combines id column and the PC columns
+pcaSpeciesData <- cbind(order1[1], pcaSpeciesData)
 
 # retrieves all PC values for PC4
 pcaSpeciesData$PC4
+
+# removes the arrows for vectors 
+biplot(pca.species, var.axes=FALSE)
+
+# new PCA graph without labels and vectors 
+testPlot <- plot(pcaSpeciesData$PC1, pcaSpeciesData$PC2, main = "Species PCA", xlab= "PC1", ylab = "PC2")
+pcaSpeciesData1 <- pcaSpeciesData[,-1]
+rownames(pcaSpeciesData1) <- pcaSpeciesData[,1]
+text(pcaSpeciesData$PC1, pcaSpeciesData$PC2, labels = row.names(pcaSpeciesData1))
+
+# Plot over the points with different responses to Q12 of Survey (Red Velvet)
+
+# Blue = Awesome 
+whichRow<-which(grepl(c("5947"), pcaSpeciesData$id, fixed = FALSE))
+test <- pcaSpeciesData[whichRow, ]
+points(test$PC1, test$PC2, col="blue", pch = 16)
+
+# Green = Good
+multiMatch <- c("2108", "5853", "7917", "1718", "5853", "3188", "5029", "4699", "6082", "7798", "3281")
+whichRow<-which(grepl(paste(multiMatch, collapse="|"), pcaSpeciesData$id, fixed = FALSE))
+test <- pcaSpeciesData[whichRow, ]
+points(test$PC1, test$PC2, col="green", pch = 16)
+
+# Yellow = Tired
+multiMatch <- c("3997", "6418", "7817", "8777", "5384", "8891", "5461", "6910", "8121", "6994", "2004", "3567", "9594", "1015", "1009")
+whichRow<-which(grepl(paste(multiMatch, collapse="|"), pcaSpeciesData$id, fixed = FALSE))
+test <- pcaSpeciesData[whichRow, ]
+points(test$PC1, test$PC2, col="yellow", pch = 16)
+
+# Orange = Sick
+multiMatch <- c("4024", "7211", "2464")
+whichRow<-which(grepl(paste(multiMatch, collapse="|"), pcaSpeciesData$id, fixed = FALSE))
+test <- pcaSpeciesData[whichRow, ]
+points(test$PC1, test$PC2, col="orange", pch = 16)
+
+# Red = Stressed 
+multiMatch <- c("5554", "3447", "4741", "6730", "1000", "9154", "1310", "3769", "3855", "3365")
+whichRow<-which(grepl(paste(multiMatch, collapse="|"), pcaSpeciesData$id, fixed = FALSE))
+test <- pcaSpeciesData[whichRow, ]
+points(test$PC1, test$PC2, col="red", pch = 16)
