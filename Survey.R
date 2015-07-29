0ba59700d8bf6d48d6208f8486e41e9f8e5c9c3c
Ninahuadiy <nhua8271@gmail.com>
2015-07-29 16:04:22 -0500

    2nd commit

--- Survey.R
+++ Survey.R
@@ -8,3 +8,14 @@ survey2.sub <- subset(survey, Cluster == 2)
 survey1.2.sub <- subset(survey, Cluster == 1.2)
 survey1.3.sub <- subset(survey, Cluster == 1.3)
 survey2.3.sub <- subset(survey, Cluster == 2.3)
+# Question 12, response 1
+surveyQ12.1.sub <- subset(survey, Q12 == 1)
+surveyQ12.2.sub <- subset(survey, Q12 == 2)
+surveyQ12.3.sub <- subset(survey, Q12 == 3)
+surveyQ12.4.sub <- subset(survey, Q12 == 4)
+surveyQ12.5.sub <- subset(survey, Q12 == 5)
+
+
+
+
+
