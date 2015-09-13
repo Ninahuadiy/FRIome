4446251ad5d1de415a72e614d60e5596b828adb3
Ninahuadiy <nhua8271@gmail.com>
2015-09-13 15:32:10 -0500

    2nd commit

--- Scores-Loading.R
+++ Scores-Loading.R
@@ -1,8 +1,11 @@
 # Makes the Species' Score plot (what variables control)
-testPlot <- plot(pcaSpeciesData$PC1, pcaSpeciesData$PC2, main = "Species PCA", xlab= "PC1", ylab = "PC2")
-pcaSpeciesData1 <- pcaSpeciesData[,-1]
-rownames(pcaSpeciesData1) <- pcaSpeciesData[,1]
-text(pcaSpeciesData$PC1, pcaSpeciesData$PC2, labels = row.names(pcaSpeciesData1))
+testPlot <- plot(surveyS1415$PC1, surveyS1415$PC2, main = "Species PCA", xlab= "PC1", ylab = "PC2")
+surveyS1415Chart <- surveyS1415[,-1]
+rownames(surveyS1415Chart) <- surveyS1415[,1]
+text(surveyS1415$PC1, surveyS1415$PC2, labels = row.names(surveyS1415Chart))
+
+# Expands the graph margins to its maximum amount
+par(mar=c(0,0,0,0)+0.1)
 
 # Makes the Species' Loading plot (variables)
 plot(pca.species$rotation, xlab = "Loadings on PC1", ylab = "Loadings on PC2", main = "Species Loading Plot")
@@ -10,8 +13,8 @@ plot(pca.species$rotation, xlab = "Loadings on PC1", ylab = "Loadings on PC2", m
 textplot(pca.species$rotation[,1], pca.species$rotation[,2], rownames(pca.species$rotation), new = TRUE, show.lines = TRUE)
 
 # Trying to play with x and y limits; not working
-mx <- apply(pca.species$rotation, 2, max)
-mn <- apply(pca.species$rotation, 2, min)
+#mx <- apply(pca.species$rotation, 2, max)
+#mn <- apply(pca.species$rotation, 2, min)
 
-textplot(pca.species$rotation[,1], pca.species$rotation[,2], rownames(pca.species$rotation), new = TRUE, show.lines = TRUE, xlim=c(mn[25], mx[25]), ylim=c(mn[25], mx[25]))
+#textplot(pca.species$rotation[,1], pca.species$rotation[,2], rownames(pca.species$rotation), new = TRUE, show.lines = TRUE, xlim=c(mn[1], mx[1]), ylim=c(mn[2], mx[2]))
 
